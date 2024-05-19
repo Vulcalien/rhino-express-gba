b7e5bc22f42f99997af85bf7325b7555e40255da
Vulcalien <vulcalien@vulcalien.net>
2024-05-19 20:21:47 +0200

    Add three sound effects

---
+++ include/sfx.h
@@ -0,0 +1,25 @@
+/* Copyright _year_ Vulcalien
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <https://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include "main.h"
+
+// include the direct sound driver needed to play music
+#include "sound.h"
+
+extern const u8 sfx_place_obstacle[1906];
+extern const u8 sfx_hit_obstacle[887];
+extern const u8 sfx_water[9107];
