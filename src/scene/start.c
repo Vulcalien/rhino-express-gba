d5cbfc89422185f1197990fa815dde7d9d01ac90
Vulcalien <vulcalien@vulcalien.net>
2024-07-07 15:36:42 +0200

    Start: draw the cutscene images as sprites

--- src/scene/start.c
+++ src/scene/start.c
@@ -15,13 +15,47 @@
  */
 #include "scene.h"
 
+#include <gba/display.h>
+#include <gba/sprite.h>
+#include <gba/dma.h>
+
+#include "screen.h"
+
+static u32 page;
+
 static void start_init(void *data) {
+    page = 0;
+
+    screen_mode_4();
 }
 
 static void start_tick(void) {
 }
 
+#include "../res/cutscenes.c"
+
+IWRAM_SECTION
 static void start_draw(void) {
+    u32 image = 0; // DEBUG
+
+    // TODO do not repeat this every draw cycle
+    dma_config(DMA3, &(struct DMA) { .chunk = DMA_CHUNK_32_BIT });
+    dma_transfer(
+        DMA3,
+        display_get_charblock(5) + 64 * 64,
+        cutscenes + (64 * 64) * image,
+        64 * 64 / 4
+    );
+
+    sprite_config(0, &(struct Sprite) {
+        .x = (240 - 64) / 2,
+        .y = (160 - 64) / 2 - 32,
+
+        .size = 3, // 64x64
+
+        .tile = 512 + 256, // TODO
+        .colors = 1
+    });
 }
 
 const struct Scene scene_start = {
