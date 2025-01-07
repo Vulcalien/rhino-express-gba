Vulcalien/rhino-express-gba

commit 3229262634a184faa9859eac82b7c286f467a6bc

Level: draw editing cursor on top of all other level sprites

src/level.c
@@ -194,6 +194,18 @@ static inline void level_init(struct Level *level,
             level->solid_entities[t][i] = LEVEL_NO_ENTITY;
 }
 
+static inline void enter_editing_mode(struct Level *level) {
+    level->editor.active = true;
+
+    level_add_edit_sidebar(level);
+    level_add_edit_cursor(level);
+
+    // set cursor position to the level's center
+    level->editor.xt = level->metadata->size.w / 2;
+    level->editor.yt = level->metadata->size.h / 2;
+
+    MUSIC_PLAY(music_editing);
+}
 
 static inline void load_tiles(struct Level *level) {
     const struct level_Metadata *metadata = level->metadata;
@@ -281,6 +293,10 @@ void level_load(struct Level *level,
     level_init(level, metadata);
     update_offset(level);
 
+    // the cursor entity needs to be added before any other entity, so
+    // that its sprites are drawn on top of all other level sprites
+    enter_editing_mode(level);
+
     load_tiles(level);
     load_mailboxes(level);
     load_decorations(level);
@@ -304,14 +320,6 @@ void level_load(struct Level *level,
     // copy 'obstacles' array
     for(u32 i = 0; i < LEVEL_OBSTACLE_TYPES; i++)
         level->editor.obstacles[i] = metadata->obstacles[i];
-
-    // enter editing mode
-    level->editor.active = true;
-    level_add_edit_sidebar(level);
-    level_add_edit_cursor(level);
-    level->editor.xt = metadata->size.w / 2;
-    level->editor.yt = metadata->size.h / 2;
-    MUSIC_PLAY(music_editing);
 
     // add player
     level_add_player(level);

commit 52ad4127cc5fa85e83c87ef370c41be232c31642

Level: fix bug causing wrong obstacles to be selected by editor
When 'level_add_edit_cursor' is called, the 'obstacles' array must
already be filled with data, so that the editor can select the first
available obstacle.

Bug introduced in commit 3229262634a184faa9859eac82b7c286f467a6bc
(Level: draw editing cursor on top of all other level sprites).

src/level.c
@@ -195,15 +195,27 @@ static inline void level_init(struct Level *level,
 }
 
 static inline void enter_editing_mode(struct Level *level) {
-    level->editor.active = true;
+    const struct level_Metadata *metadata = level->metadata;
+
+    // load edit sidebar
+    memory_copy_32(
+        (vu8 *) display_charblock(4) + 128 * 32,
+        level_sidebar,
+        4 * 8 * 32
+    );
+
+    // copy 'obstacles' array
+    for(u32 i = 0; i < LEVEL_OBSTACLE_TYPES; i++)
+        level->editor.obstacles[i] = metadata->obstacles[i];
 
     level_add_edit_sidebar(level);
     level_add_edit_cursor(level);
 
     // set cursor position to the level's center
-    level->editor.xt = level->metadata->size.w / 2;
-    level->editor.yt = level->metadata->size.h / 2;
+    level->editor.xt = metadata->size.w / 2;
+    level->editor.yt = metadata->size.h / 2;
 
+    level->editor.active = true;
     MUSIC_PLAY(music_editing);
 }
 
@@ -309,17 +321,6 @@ void level_load(struct Level *level,
             48 * 32
         );
     }
-
-    // load edit sidebar
-    memory_copy_32(
-        (vu8 *) display_charblock(4) + 128 * 32,
-        level_sidebar,
-        4 * 8 * 32
-    );
-
-    // copy 'obstacles' array
-    for(u32 i = 0; i < LEVEL_OBSTACLE_TYPES; i++)
-        level->editor.obstacles[i] = metadata->obstacles[i];
 
     // add player
     level_add_player(level);